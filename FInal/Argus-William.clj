60b8cab60f9a7c209d9e3a969a8885cb63293b5f
warg15 <63023642+warg15@users.noreply.github.com>
2021-01-27 23:48:08 -0800

    Delete Argus-William.clj

--- FInal/Argus-William.clj
+++
@@ -1,9 +0,0 @@
-;;William Argus
-;;A12802324
-;;Collaborated with Colman Dekker via a phone call in which we discussed the
-;;questions to ensure mutual understanding and gave guidance to each other in debugging
-;;The code was mainly done individually with us just sort of checking up with each other after
-;;each problem to make sure we agreed on directions and general approach 
-;;Thanks to the proffessor
-;;and TA's for an interesting
-;;and well run class
